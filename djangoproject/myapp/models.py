f405408ead95dedf537022afa3e2e108e1a317d0
MatiasCabelloV <m.cabellovillarreal@uandresbello.edu>
2023-09-25 23:47:13 -0300

    actualizacion user

--- djangoproject/myapp/models.py
+++ djangoproject/myapp/models.py
@@ -9,3 +9,9 @@ class tareas(models.Model):
     titulo = models.CharField(max_length=200)
     descripcion = models.TextField()
     proyecto = models.ForeignKey(project, on_delete=models.CASCADE)
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    rut = models.CharField(max_length=20)
+    correo = models.CharField(max_length=255)
+    clave = models.CharField(max_length=255)
