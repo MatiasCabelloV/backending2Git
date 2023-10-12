cae25b7df8d2c90b39e571208d3e99863567b3d0
MatiasCabelloV <m.cabellovillarreal@uandresbello.edu>
2023-10-12 19:59:11 -0300

    Integracion APIs

---
+++ djangoproject/myapp/serializer.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from .models import *
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+class TareaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tarea
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class ProfesorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profesor
+        fields = '__all__'
+
+class HorarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Horario
+        fields = '__all__'
+
+class SecretarioAcademicoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SecretarioAcademico
+        fields = '__all__'
+
+class AdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Admin
+        fields = '__all__'
