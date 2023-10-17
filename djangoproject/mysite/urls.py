1b24dcf3a6df94f4e6021f1ba1a4d5297060cb86
Javier <j.gavilnorellana@uandresbello.edu>
2023-10-17 15:08:47 -0300

    desconozco

--- djangoproject/mysite/urls.py
+++ djangoproject/mysite/urls.py
@@ -4,5 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
-    path('accounts/', include('django.contrib.auth.urls'))
 ]
